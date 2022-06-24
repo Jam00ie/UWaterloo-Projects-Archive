5f7d0290d73f6c9f1eb322b507d14bdd460722da
Jamie Kang <j228kang@uwaterloo.ca>
2022-06-24 13:00:39 -0400

    heroku testing

--- src/index.mjs
+++ src/index.mjs
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import data from "./data.js";
 const app = express();
-const { PORT = 3000 } = process.env;
+
+let PORT = process.env.PORT;
+if (PORT == null || PORT == "") {
+  PORT = 3000;
+}
 
 app.use(bodyParser.json()).use(cors())
 
 app.get("/", (request, response) => response.send("Hello World!"));
 
-app.listen(PORT, () =>
-    console.log(`Hello World, I'm listening http://localhost:${PORT}`)
-);
+app.listen(PORT);
