dc7a980cc453d9240356547c354f72898ebe866e
Jamie Kang <j228kang@uwaterloo.ca>
2022-06-24 13:21:40 -0400

    .

--- src/index.mjs
+++ src/index.mjs
@@ -2,15 +2,12 @@ import express from "express"; //importing a module
 import bodyParser from "body-parser";
 import cors from "cors";
 import data from "./data.js";
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-let PORT = process.env.PORT;
-if (PORT == null || PORT == "") {
-  PORT = 3000;
-}
-
-app.use(bodyParser.json()).use(cors())
+app.use(bodyParser.json()).use(cors());
 
 app.get("/", (request, response) => response.send("Hello World!"));
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
