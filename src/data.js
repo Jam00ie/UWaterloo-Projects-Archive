a26ff7959f351c51e2517a1f0b23a8ee2323848f
Jamie Kang <j228kang@uwaterloo.ca>
2022-06-27 19:50:32 -0400

    getprofessors

--- src/data.js
+++ src/data.js
@@ -1,44 +1,110 @@
-module.exports ={
-    Professor: [
-    {
-        id: "1",
-        name: "Carrie Mitchell",
-        course: "PLAN 102",
-        email: "carrie.mitchell@uwaterloo.ca",
-        office: "EV3 3317",
-        Hour: "0900-1200 Mon-Fri"
-    },
-    {
-        id: "2",
-        name: "Markus Moos",
-        course: "PLAN 103",
-        email: "mmoos@uwaterloo.ca",
-        office: "EV3 3306",
-        Hour: "1200-1500 Mon-Fri"
-    },
-    {
-        id: "3",
-        name: "Jennifer Deen",
-        course: "PLAN 233",
-        email: "jennifer.dean@uwaterloo.ca",
-        office: "EV3 3221",
-        Hour: "0900-1200 Mon-Fri"
-    },
-    {
-        id: "4",
-        name: "John L. Lewis",
-        course: "PLAN 309",
-        email: "j7lewis@uwaterloo.ca",
-        office: "EV3 3241",
-        Hour: "1000-1300 Mon-Fri"
-    },
-    {
-        id: "5",
-        name: "Joe Qian",
-        course: "PLAN 261",
-        email: "z3qian@uwaterloo.ca",
-        office: "EV3 3253",
-        Hour: "1200-1500 Mon-Fri"
-    },
-    ]
+const data = {
+    professors: [
+        {
+            id: 1,
+            name: 'Carrie Mitchell',
+            email: 'cmitchell@uwaterloo.ca',
+            course_1: 'PLAN 102',
+            course_2: 'PLAN 300',
+            office: 'EV3 3317',
+            hours: '0900-1200 Mon-Fri'
+        },
+        {
+            id: 2,
+            name: 'Markus Moos',
+            email: 'mmoos@uwaterloo.ca',
+            course_1: 'MATH 135',
+            course_2: 'N/A',
+            office: 'M3 3306',
+            hours: '1200-1500 Mon-Fri'
+        },
+        {
+            id: 3,
+            name: 'Jennifer Deen',
+            email: 'jdeen@uwaterloo.ca',
+            course_1: 'ECON 101',
+            course_2: 'MSCI 261',
+            office: 'EV3 3221',
+            hours: '0900-1200 Mon-Fri'
+        },
+        {
+            id: 4,
+            name: 'John L. Lewis',
+            email: 'j7lewis@uwaterloo.ca',
+            course_1: 'SYDE 101L',
+            course_2: 'BME 101L',
+            office: 'E5 6012',
+            hours: '1000-1300 Mon-Fri'
+        },
+        {
+            id: 5,
+            name: 'Joe Qian',
+            email: 'z3qian@uwaterloo.ca',
+            course_1: 'CHEM 266',
+            course_2: 'N/A',
+            office: 'STC 1253',
+            hours: '1200-1500 Mon-Fri'
+        },
+    ],
+
+    courses: [
+        {
+            id: 1,
+            course_code: 'PLAN 102',
+            course_name: 'Professional Communication',
+            professor: 'Carrie Mitchell',
+            location: 'online'
+        },
+        {
+            id: 2,
+            course_code: 'PLAN 300',
+            course_name: 'Planning Theory',
+            professor: 'Carrie Mitchell',
+            location: 'online'
+        },
+        {
+            id: 3,
+            course_code: 'MATH 135',
+            course_name: 'Algebra for Honours Mathematics',
+            professor: 'Markus Moos',
+            location: 'MC 4041'
+        },
+        {
+            id: 4,
+            course_code: 'ECON 101',
+            course_name: 'Introduction to Microeconomics',
+            professor: 'Jennifer Deen',
+            location: 'DC 1350'
+        },
+        {
+            id: 5, 
+            course_code: 'MSCI 261',
+            course_name: 'Engineering Economics: Financial Management for Engineers',
+            professor: 'Jennifer Deen',
+            location: 'E6 2024'
+        },
+        {
+            id: 6,
+            course_code: 'SYDE 101L',
+            course_name: 'Communications in Systems Design Engineering-Visualization',
+            professor: 'John L. Lewis',
+            location: 'E5 6008'
+        },
+        {
+            id: 7,
+            course_code: 'BME 101L',
+            course_name: '	Communications in Biomedical Engineering-Visualization',
+            professor: 'John L. Lewis',
+            location: 'E5 6008'
+        },
+        {
+            id: 8,
+            course_code: 'CHEM 266',
+            course_name: 'Basic Organic Chemistry 1',
+            professor: 'Joe Qian',
+            location: 'M3 1006'
+        },
+    ],
 }
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import express from "express"; //importing a module
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-//import data from "./data.js";
+import data from "./data.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json()).use(cors());
 
 app.get("/", (request, response) => response.send("Hello World!!"));
+
+app.get("/professors", (req, res) => {
+    res.json(data.professors);
+}
+);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
