b850d1f2e502233335e47b8fb5985dfe930e91ae
s7kalia <107931259+s7kalia@users.noreply.github.com>
2022-06-28 03:55:02 +0200

    Update profcours.js

--- src/components/profcours.js
+++ src/components/profcours.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ProfCours extents Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return <h2> Professors and Courses List </h2>;
+  }
+}
 
