fd5e5e29350b7b3b35211bb64f2f1b68e4799811
s7kalia <107931259+s7kalia@users.noreply.github.com>
2022-06-28 22:30:52 +0200

    Update CourseListItem.js

--- src/components/CourseListItem.js
+++ src/components/CourseListItem.js
@@ -1,1 +1,32 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
+function CourseListItem({ id, course_code}) {
+
+    const [details, setDetails] = useState(null);
+
+    return (
+        <div>
+            <button
+                onClick = {() => {
+                    fetch(`https://weaccelerate-backend.herokuapp.com/courses/${id}`)
+                    .then ((response) => response.json())
+                    .then ((response) => setDetails(response));
+                }}>
+                {course_code}
+            </button>
+            {details && (
+                <div>
+                    <div> {details.course_code}</div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+CourseListItem.propTypes = {
+    id: PropTypes.string.isRequired,
+    course_code: PropTypes.string.isRequired,
+};
+
+export default CourseListItem;
