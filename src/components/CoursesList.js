0ad5824986142b17d3c6491651c31c93a4e168e9
s7kalia <107931259+s7kalia@users.noreply.github.com>
2022-06-28 22:51:23 +0200

    Update CoursesList.js

--- src/components/CoursesList.js
+++ src/components/CoursesList.js
@@ -15,10 +15,13 @@ export default class CoursesList extends Component {
     }
 
     renderCourses() {
-        return this.state.courses.map(course => (
-            <div key={course.id}>{course.course_code}</div>
-        ));
+        return this.state.courses.map(course => <CourseListItem key={course.id} id={course.id} course_code={course.course_code} />);
     }
+    
+    renderCourseDetails() {
+        if (!details) { return; } 
+        return <CourseDetails course_name={details.course_name} professor={details.professor} location={details.location} />
+        }
     
     render() {
         return (
