1b4488f9f6425e004462a9ed993e4d5ca69a870e
Jamie Kang <j228kang@uwaterloo.ca>
2022-06-27 20:35:40 -0400

    prof course id id

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import data from "./data.js";
+import * as utilities from "./functions.js";
+import {isInvalidId} from "./functions.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +13,37 @@ app.get("/", (request, response) => response.send("Hello World!!"));
 
 app.get("/professors", (req, res) => {
     res.json(data.professors);
+}
+);
+
+app.get("/professors/:id", (req, res) => {
+    if (utilities.isInvalidId(req.params.id)) {
+        return res.status(400).json({ error: "Invalid id." })
+    }
+    const id = parseInt(req.params.id);
+    const professor = data.professors.find((prof) => prof.id === id);
+    if (!professor) {
+        res.status(404).json({error: "Professor not found."});
+    }
+    return res.json(professor);
+}
+);
+
+app.get("/courses", (req, res) => {
+    res.json(data.courses);
+}
+);
+
+app.get("/courses/:id", (req, res) => {
+    if (utilities.isInvalidId(req.params.id)) {
+        return res.status(400).json({ error: "Invalid id." })
+    }
+    const id = parseInt(req.params.id);
+    const course = data.courses.find((cour) => cour.id === id);
+    if (!course) {
+        res.status(404).json({error: "Course not found."});
+    }
+    return res.json(course);
 }
 );
 
